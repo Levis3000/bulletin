18204507e983bbfdf8bf4312cf3d3aa6694f5106
Levis3000 <levis300@gmail.com>
2025-10-29 23:33:03 -0500

    addded placeholder variavles

---
+++ app/whop-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { WhopIframeSdkProvider } from "@whop/react";
+
+type Props = { children: React.ReactNode };
+
+export function WhopProvider({ children }: Props) {
+  const appId = process.env.NEXT_PUBLIC_WHOP_APP_ID || "dev_app_placeholder";
+  return <WhopIframeSdkProvider appId={appId}>{children}</WhopIframeSdkProvider>;
+}
+
+
